2fc933dc838c8aa2a4cb490c26fab746052b3a18
Strflightmight09 <ezragryn57@gmail.com>
2024-05-05 14:13:44 +0300

    Fixed folder location

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -10,6 +10,7 @@ import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.Encoder;
 
 public class IntakeConstants {
 
